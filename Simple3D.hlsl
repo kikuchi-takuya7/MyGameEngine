e878ccb4edcb7ead52b759395215446f913cf3d7
巧椰 菊地 <220065@jc-21.jp>
2023-06-14 12:59:35 +0900

    ああ

--- Simple3D.hlsl
+++ Simple3D.hlsl
@@ -57,5 +57,5 @@ float4 PS(VS_OUT inData) : SV_Target
 	float4 ambentSource = (0.2, 0.2, 0.2, 1.0); //明るくすればツルツルする
 	float4 diffuse = lightSource * g_texture.Sample(g_sampler, inData.uv) * inData.color;//拡散反射色
 	float4 ambient = lightSource * g_texture.Sample(g_sampler, inData.uv) * ambentSource;//環境反射色
-	return (diffuse); //実際の色
+	return (diffuse + ambient); //実際の色
 }
