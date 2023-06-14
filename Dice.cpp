56ab06ec843d7009903ffffbb87c2d649d59b9fb
巧椰 菊地 <220065@jc-21.jp>
2023-06-14 12:59:28 +0900

    色々変えた

--- Dice.cpp
+++ Dice.cpp
@@ -21,33 +21,33 @@ void Dice::MakeVerBf()
 		//参考　https://gihyo.jp/dev/serial/01/as3/0049
 		// https://shibuya24.info/entry/unity-mesh-dynamic-apply-texture
 		//pingの画像見ながらやったらわかりやすい
-		{XMVectorSet(-1.0f,  1.0f, 0.0f, 0.0f), XMVectorSet(0.0f, 0.0f, 0.0f, 0.0f) , XMVectorSet(0.0f, 0.0f,0.0f, 0.0f) },	// 四角形の頂点（左上）
-		{XMVectorSet(1.0f, 1.0f, 0.0f, 0.0f),   XMVectorSet(0.25f, 0.0f, 0.0f, 0.0f), XMVectorSet(0.0f, 0.0f,0.0f, 0.0f) },	// 四角形の頂点（右上）
-		{XMVectorSet(1.0f, -1.0f, 0.0f, 0.0f),  XMVectorSet(0.25f, 0.5f, 0.0f, 0.0f), XMVectorSet(0.0f, 0.0f,0.0f, 0.0f) },	// 四角形の頂点（右下）
-		{XMVectorSet(-1.0f, -1.0f, 0.0f, 0.0f), XMVectorSet(0.0f, 0.5f, 0.0f, 0.0f),  XMVectorSet(0.0f, 0.0f,0.0f, 0.0f) },	// 四角形の頂点（左下）		
-		{XMVectorSet(-1.0f, 1.0f, 2.0f, 0.0f),  XMVectorSet(0.75f, 0.0f, 0.0f, 0.0f), XMVectorSet(0.0f, 0.0f,0.0f, 0.0f) },	//4  使ってない
-		{XMVectorSet(1.0f, 1.0f, 2.0f, 0.0f),   XMVectorSet(0.5f, 0.0f, 0.0f, 0.0f),  XMVectorSet(0.0f, 0.0f,0.0f, 0.0f) },	//5
-		{XMVectorSet(1.0f, -1.0f, 2.0f, 0.0f),  XMVectorSet(0.5f, 0.5f, 0.0f, 0.0f),  XMVectorSet(0.0f, 0.0f,0.0f, 0.0f) },	//6
-		{XMVectorSet(-1.0f, -1.0f, 2.0f, 0.0f), XMVectorSet(0.75f, 0.5f, 0.0f, 0.0f), XMVectorSet(0.0f, 0.0f,0.0f, 0.0f) },	//7  使ってない
+		{XMVectorSet(-1.0f,  1.0f, 0.0f, 0.0f), XMVectorSet(0.0f, 0.0f, 0.0f, 0.0f) , XMVectorSet(0.0f, 0.0f,-1.0f, 0.0f) },	// 四角形の頂点（左上）
+		{XMVectorSet(1.0f, 1.0f, 0.0f, 0.0f),   XMVectorSet(0.25f, 0.0f, 0.0f, 0.0f), XMVectorSet(0.0f, 0.0f,-1.0f, 0.0f) },	// 四角形の頂点（右上）
+		{XMVectorSet(1.0f, -1.0f, 0.0f, 0.0f),  XMVectorSet(0.25f, 0.5f, 0.0f, 0.0f), XMVectorSet(0.0f, 0.0f,-1.0f, 0.0f) },	// 四角形の頂点（右下）
+		{XMVectorSet(-1.0f, -1.0f, 0.0f, 0.0f), XMVectorSet(0.0f, 0.5f, 0.0f, 0.0f),  XMVectorSet(0.0f, 0.0f,-1.0f, 0.0f) },	// 四角形の頂点（左下）		
+		{XMVectorSet(-1.0f, 1.0f, 2.0f, 0.0f),  XMVectorSet(0.75f, 0.0f, 0.0f, 0.0f), XMVectorSet(0.0f, 0.0f,-1.0f, 0.0f) },	//4  使ってない
+		{XMVectorSet(1.0f, 1.0f, 2.0f, 0.0f),   XMVectorSet(0.5f, 0.0f, 0.0f, 0.0f),  XMVectorSet(0.0f, 0.0f,1.0f, 0.0f) },	//5
+		{XMVectorSet(1.0f, -1.0f, 2.0f, 0.0f),  XMVectorSet(0.5f, 0.5f, 0.0f, 0.0f),  XMVectorSet(0.0f, 0.0f,1.0f, 0.0f) },	//6
+		{XMVectorSet(-1.0f, -1.0f, 2.0f, 0.0f), XMVectorSet(0.75f, 0.5f, 0.0f, 0.0f), XMVectorSet(0.0f, 0.0f,-1.0f, 0.0f) },	//7  使ってない
 		//側面用
 		//２と６の境界と６と５の境界
-		{XMVectorSet(-1.0f, 1.0f, 2.0f, 0.0f),  XMVectorSet(0.25f, 0.5f, 0.0f, 0.0f), XMVectorSet(0.0f, 0.0f,0.0f, 0.0f) },	//8  4
-		{XMVectorSet(1.0f, 1.0f, 2.0f, 0.0f),   XMVectorSet(0.5f, 0.5f, 0.0f, 0.0f),  XMVectorSet(0.0f, 0.0f,0.0f, 0.0f) },	//9  5
-		{XMVectorSet(1.0f, -1.0f, 2.0f, 0.0f),  XMVectorSet(0.5f, 1.0f, 0.0f, 0.0f),  XMVectorSet(0.0f, 0.0f,0.0f, 0.0f) },	//10 6
-		{XMVectorSet(-1.0f, -1.0f, 2.0f, 0.0f), XMVectorSet(0.25f, 1.0f, 0.0f, 0.0f), XMVectorSet(0.0f, 0.0f,0.0f, 0.0f) },	//11 7
+		{XMVectorSet(-1.0f, 1.0f, 2.0f, 0.0f),  XMVectorSet(0.25f, 0.5f, 0.0f, 0.0f), XMVectorSet(0.0f, 0.0f,1.0f, 0.0f) },	//8  4
+		{XMVectorSet(1.0f, 1.0f, 2.0f, 0.0f),   XMVectorSet(0.5f, 0.5f, 0.0f, 0.0f),  XMVectorSet(0.0f, 0.0f,1.0f, 0.0f) },	//9  5
+		{XMVectorSet(1.0f, -1.0f, 2.0f, 0.0f),  XMVectorSet(0.5f, 1.0f, 0.0f, 0.0f),  XMVectorSet(0.0f, 0.0f,1.0f, 0.0f) },	//10 6
+		{XMVectorSet(-1.0f, -1.0f, 2.0f, 0.0f), XMVectorSet(0.25f, 1.0f, 0.0f, 0.0f), XMVectorSet(0.0f, 0.0f,1.0f, 0.0f) },	//11 7
 		//１と５の境界
-		{XMVectorSet(-1.0f,  1.0f, 0.0f, 0.0f), XMVectorSet(0.0f, 0.5f, 0.0f, 0.0f),  XMVectorSet(0.0f, 0.0f,0.0f, 0.0f) },	//12 0
-		{XMVectorSet(-1.0f, -1.0f, 0.0f, 0.0f), XMVectorSet(0.0f, 1.0f, 0.0f, 0.0f),  XMVectorSet(0.0f, 0.0f,0.0f, 0.0f) },	//13 3
+		{XMVectorSet(-1.0f,  1.0f, 0.0f, 0.0f), XMVectorSet(0.0f, 0.5f, 0.0f, 0.0f),  XMVectorSet(0.0f, 0.0f,-1.0f, 0.0f) },	//12 0
+		{XMVectorSet(-1.0f, -1.0f, 0.0f, 0.0f), XMVectorSet(0.0f, 1.0f, 0.0f, 0.0f),  XMVectorSet(0.0f, 0.0f,-1.0f, 0.0f) },	//13 3
 		//上面用
-		{XMVectorSet(-1.0f,  1.0f, 0.0f, 0.0f), XMVectorSet(0.75f, 0.0f, 0.0f, 0.0f), XMVectorSet(0.0f, 0.0f,0.0f, 0.0f) },	//14 0
-		{XMVectorSet(1.0f, 1.0f, 0.0f, 0.0f),   XMVectorSet(0.5f, 0.0f, 0.0f, 0.0f),  XMVectorSet(0.0f, 0.0f,0.0f, 0.0f) },	//15 1
-		{XMVectorSet(-1.0f, 1.0f, 2.0f, 0.0f),  XMVectorSet(0.75f, 0.5f, 0.0f, 0.0f), XMVectorSet(0.0f, 0.0f,0.0f, 0.0f) },	//16 4
-		{XMVectorSet(1.0f, 1.0f, 2.0f, 0.0f),   XMVectorSet(0.5f, 0.5f, 0.0f, 0.0f),  XMVectorSet(0.0f, 0.0f,0.0f, 0.0f) },	//17 5
+		{XMVectorSet(-1.0f,  1.0f, 0.0f, 0.0f), XMVectorSet(0.75f, 0.0f, 0.0f, 0.0f), XMVectorSet(0.0f, 0.0f,-1.0f, 0.0f) },	//14 0
+		{XMVectorSet(1.0f, 1.0f, 0.0f, 0.0f),   XMVectorSet(0.5f, 0.0f, 0.0f, 0.0f),  XMVectorSet(0.0f, 0.0f,-1.0f, 0.0f) },	//15 1
+		{XMVectorSet(-1.0f, 1.0f, 2.0f, 0.0f),  XMVectorSet(0.75f, 0.5f, 0.0f, 0.0f), XMVectorSet(0.0f, 0.0f,1.0f, 0.0f) },	//16 4
+		{XMVectorSet(1.0f, 1.0f, 2.0f, 0.0f),   XMVectorSet(0.5f, 0.5f, 0.0f, 0.0f),  XMVectorSet(0.0f, 0.0f,1.0f, 0.0f) },	//17 5
 		//下面用
-		{XMVectorSet(1.0f, -1.0f, 0.0f, 0.0f),  XMVectorSet(1.0f, 0.0f, 0.0f, 0.0f),  XMVectorSet(0.0f, 0.0f,0.0f, 0.0f) },	//18 2
-		{XMVectorSet(-1.0f, -1.0f, 0.0f, 0.0f), XMVectorSet(0.75f, 0.0f, 0.0f, 0.0f), XMVectorSet(0.0f, 0.0f,0.0f, 0.0f) },	//19 3
-		{XMVectorSet(1.0f, -1.0f, 2.0f, 0.0f),  XMVectorSet(1.0f, 0.5f, 0.0f, 0.0f),  XMVectorSet(0.0f, 0.0f,0.0f, 0.0f) },	//20 6
-		{XMVectorSet(-1.0f, -1.0f, 2.0f, 0.0f), XMVectorSet(0.75f, 0.5f, 0.0f, 0.0f), XMVectorSet(0.0f, 0.0f,0.0f, 0.0f) },	//21 7
+		{XMVectorSet(1.0f, -1.0f, 0.0f, 0.0f),  XMVectorSet(1.0f, 0.0f, 0.0f, 0.0f),  XMVectorSet(0.0f, 0.0f,-1.0f, 0.0f) },	//18 2
+		{XMVectorSet(-1.0f, -1.0f, 0.0f, 0.0f), XMVectorSet(0.75f, 0.0f, 0.0f, 0.0f), XMVectorSet(0.0f, 0.0f,-1.0f, 0.0f) },	//19 3
+		{XMVectorSet(1.0f, -1.0f, 2.0f, 0.0f),  XMVectorSet(1.0f, 0.5f, 0.0f, 0.0f),  XMVectorSet(0.0f, 0.0f,1.0f, 0.0f) },	//20 6
+		{XMVectorSet(-1.0f, -1.0f, 2.0f, 0.0f), XMVectorSet(0.75f, 0.5f, 0.0f, 0.0f), XMVectorSet(0.0f, 0.0f,1.0f, 0.0f) },	//21 7
 	};
 
 	// 頂点データ用バッファの設定
--- Simple3D.hlsl
+++ Simple3D.hlsl
@@ -40,7 +40,7 @@ VS_OUT VS(float4 pos : POSITION, float4 uv : TEXCOORD, float4 normal : NORMAL)
 	//法線を回転
 	normal = mul(normal, matW);
 
-	float4 light = float4(0, 0, -0.8, 0);//光が出てくる方向。こっち向いてるタイプ
+	float4 light = float4(-1, 0.5, -0.7, 0);//光が出てくる方向。こっち向いてるタイプ
 	light = normalize(light);
 	outData.color = clamp(dot(normal, light), 0, 1);
 
@@ -53,9 +53,9 @@ VS_OUT VS(float4 pos : POSITION, float4 uv : TEXCOORD, float4 normal : NORMAL)
 //───────────────────────────────────────
 float4 PS(VS_OUT inData) : SV_Target
 {
-	float4 lightSource = float4(1.0,1.0,1.0,0.0);//色の4原色
+	float4 lightSource = float4(1.0,1.0,1.0,1.0);//色の4原色
 	float4 ambentSource = (0.2, 0.2, 0.2, 1.0); //明るくすればツルツルする
 	float4 diffuse = lightSource * g_texture.Sample(g_sampler, inData.uv) * inData.color;//拡散反射色
 	float4 ambient = lightSource * g_texture.Sample(g_sampler, inData.uv) * ambentSource;//環境反射色
-	return (diffuse + ambient); //実際の色
+	return (diffuse); //実際の色
 }
